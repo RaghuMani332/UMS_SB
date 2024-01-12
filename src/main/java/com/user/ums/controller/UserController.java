7b8dc52882ee2afa19f001c647d21a78205551e2
PREMKUMAR ANBU <139702919+PremAnbu@users.noreply.github.com>
2024-01-12 19:46:14 +0530

    Add files via upload
    Basic CRUD operation

---
+++ src/main/java/com/user/ums/controller/UserController.java
@@ -0,0 +1,44 @@
+package com.user.ums.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.user.ums.entity.User;
+import com.user.ums.requestdto.UserRequest;
+import com.user.ums.responsedto.UserResponse;
+import com.user.ums.service.UserService;
+import com.user.ums.util.ResponseStructure;
+
+@RestController
+public class UserController {
+	@Autowired
+	private UserService userService;
+	
+	@PostMapping("/users")
+	public ResponseEntity<ResponseStructure<UserResponse>> saveUser(@RequestBody UserRequest userRequest) {
+		return userService.saveUser(userRequest);
+	}
+    
+	@PutMapping("/users/{userId}")
+	public ResponseEntity<ResponseStructure<UserResponse>> updateUser(@RequestBody UserRequest userRequest,@PathVariable int userId) {
+		return userService.updateUser(userRequest,userId);
+	}
+	
+	@GetMapping("/users/{userId}")
+	public ResponseEntity<ResponseStructure<UserResponse>> getUserById(@PathVariable int userId) {
+		return userService.getUserById(userId);
+	}
+	
+	@DeleteMapping("/users/{userId}")
+	public ResponseEntity<ResponseStructure<UserResponse>> deleteUserById(@PathVariable int userId) {
+		return userService.deleteUserById(userId);
+	}
+
+}
---
+++ src/main/java/com/user/ums/util/ResponseStructure.java
@@ -0,0 +1,18 @@
+package com.user.ums.util;
+
+import org.springframework.stereotype.Controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Controller
+public class ResponseStructure<T> {
+	
+	private int status;
+	private String message;
+	private T data;
+	
+
+}
